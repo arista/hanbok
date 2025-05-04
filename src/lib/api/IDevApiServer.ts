7c80668802ec5b888a988ccf873d51500cbed6f2
arista <arista@abramsons.info>
2025-05-04 18:52:48 +0000

    pass through webappApiEndpoint and routerBase

--- src/lib/api/IDevApiServer.ts
+++ src/lib/api/IDevApiServer.ts
@@ -3,3 +3,8 @@ import type {IRouter} from "./IRouter"
 export interface IDevApiServer {
   addRoutes(prefix: string, router: IRouter): void
 }
+
+export type DevApiServerCreateFunc = (props: {
+  webappApiEndpoint: string
+  routerBase: string
+})=>IDevApiServer
