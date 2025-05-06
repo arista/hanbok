5829e346229808af84fca6058fdb088fe62024c2
arista <arista@abramsons.info>
2025-05-06 14:44:05 +0000

    removed preview

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,12 @@
 import {Command as SampleCommand} from "@cli/commands/sample"
 import {Command as BuildCommand} from "@cli/commands/build"
 import {Command as DevCommand} from "@cli/commands/dev"
-import {Command as PreviewCommand} from "@cli/commands/preview"
 import {Command as PrettierCommand} from "@cli/commands/prettier"
 import {Command as ApiServerCommand} from "@cli/commands/api-server"
 
 export const COMMANDS = {
   build: BuildCommand,
   dev: DevCommand,
-  preview: PreviewCommand,
   prettier: PrettierCommand,
   "api-server": ApiServerCommand,
   sample: SampleCommand,
