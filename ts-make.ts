dedafd064185d7eb4d72ac2ae3aea991bc99b000
arista <arista@abramsons.info>
2025-10-08 21:14:36 +0000

    updated ts-make.ts

--- ts-make.ts
+++ ts-make.ts
@@ -1,16 +1,17 @@
 const config = {
-  targets: {
-    default: {
-      depends: ["cli"],
-    },
-    cli: {
-      build: {
-        type: "esbuild",
+  plugins: [
+    {name: "ts-make/plugins-esbuild", alias: "esbuild"},
+  ],
+  targets: [
+    {name: "default", deps: ["cli", "a"]},
+    {name: "cli", deps: "a"},
+    {
+      name: "a", action: "esbuild:esbuild", args: {
         source: "src/cli.ts",
         destPrefix: "dist/cli/cli",
       }
     },
-  }
+  ],
 }
 
 export default config
