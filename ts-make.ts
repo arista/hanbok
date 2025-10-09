85850f83343529de97b49e6882cd600bdf147d9b
arista <arista@abramsons.info>
2025-10-09 20:58:10 +0000

    updated the esbuild target

--- ts-make.ts
+++ ts-make.ts
@@ -1,16 +1,33 @@
+function esbuildTarget(name: string, source: string, destPrefix: string) {
+  return {
+    name,
+    action: "esbuild:esbuild",
+    args: {
+      source,
+      destPrefix,
+      compilerOptions: {
+        paths: {
+          "@lib/*": ["./src/lib/*"],
+          "@cli/*": ["./src/cli/*"],
+          "@test/*": ["./test/*"]
+        },
+      },
+    }
+  }
+}
+
 const config = {
   plugins: [
     {name: "ts-make/plugins-esbuild", alias: "esbuild"},
   ],
   targets: [
-    {name: "default", deps: ["cli", "a"]},
-    {name: "cli", deps: "a"},
-    {
-      name: "a", action: "esbuild:esbuild", args: {
-        source: "src/cli.ts",
-        destPrefix: "dist/cli/cli",
-      }
-    },
+    {name: "default", deps: "esbuilds"},
+    {name: "esbuilds", deps: ["cli", "tools", "server", "routes", "tests"]},
+    esbuildTarget("cli", "src/cli.ts", "dist/cli/cli"),
+    esbuildTarget("tools", "src/tools.ts", "dist/tools/tools"),
+    esbuildTarget("server", "src/server.ts", "dist/server/server"),
+    esbuildTarget("routes", "src/routes.ts", "dist/routes/routes"),
+    esbuildTarget("tests", "test/tests.ts", "build/test/tests"),
   ],
 }
 
